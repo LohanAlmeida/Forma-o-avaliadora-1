87b0b9b3dae4ba2b243f0108cea65c83d373082e
Almeida Dev <150105725+LohanAlmeida@users.noreply.github.com>
2026-03-27 19:26:59 -0300

    Create questao1.py

---
+++ questao1.py
@@ -0,0 +1,8 @@
+# Entrada de dados
+celsius = float(input("Digite a temperatura em Celsius: "))
+
+# Conversão
+fahrenheit = (9 * celsius) / 5 + 32
+
+# Saída
+print(f"Temperatura em Fahrenheit: {fahrenheit:.2f}°F")
